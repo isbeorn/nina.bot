226e0abf2c5899eb983e1f9efa45694402ad24a2
Stefan Berg <isbeorn86@googlemail.com>
2019-12-27 22:01:55 +0100

    !conform command

--- commands/HelpCommand.js
+++ commands/HelpCommand.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const BaseCommand = require('./BaseCommand');
 
 class HelpCommand extends BaseCommand {
@@ -38,8 +39,26 @@ class HelpCommand extends BaseCommand {
         message.reply('https://nighttime-imaging.eu/docs/develop/site/troubleshooting/');
         break;
       }
+      case '!conform': {
+        const embed = new Discord.RichEmbed()
+        .setTitle('ASCOM Conformance check')          
+        .setURL('https://github.com/ASCOMInitiative/Conform/releases/')
+        .setThumbnail('https://avatars3.githubusercontent.com/u/38851363?s=400&v=4')
+        .setColor('0x00AE86')
+        .setDescription(`
+        Some ASCOM drivers do not conform to the ASCOM interface.          
+        This tool performs a comprehensive set of tests on a driver to determine its conformance with the relevant ASCOM interface standard as well as tests to some aspects of driver behavior against the reference implementation. 
+        It is adviced to run the conformance checker against those drivers and report issues to the driver manufacturer to fix these issues.
+        `)
+        .setFooter('Make sure to select the correct driver prior to testing under Options->Select Driver')
+        message.reply(embed);
+        break;
+      }
       case '!help': {
-        message.reply('Available commands: !donate, !logs, !docs, !devdocs, !tracker, !issue, !repository, !nina, !nina.docs, !troubleshoot, !help');
+
+        
+
+        message.reply('Available commands: !donate, !logs, !docs, !devdocs, !tracker, !issue, !repository, !nina, !nina.docs, !troubleshoot, !help, !conform');
         break;
       }
       default:
