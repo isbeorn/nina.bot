a7bdfecb8e7a58a23cf08e8a5291eab1625d7ec0
Stefan Berg <isbeorn86@googlemail.com>
2020-06-04 23:35:26 +0200

    temperature tofixed in condition

--- commands/AFGraphCommand.js
+++ commands/AFGraphCommand.js
@@ -191,13 +191,13 @@ class AFGraphCommand extends BaseCommand {
                     const date = new Date(autoFocusData.Timestamp);
                     const temperature = "n.A.";
                     if(autoFocusData.Temperature !== "NaN") {
-                        temperature = autoFocusData.Temperature;
+                        temperature = autoFocusData.Temperature.toFixed(2);
                     }
                     const embed = new Discord.RichEmbed();
                     embed.attachFile('./output.png')
                         .addField('Method', autoFocusData.Method, true )
                         .addField('Fitting', autoFocusData.Fitting, true)
-                        .addField('Temperature', temperature.toFixed(2), true)
+                        .addField('Temperature', temperature, true)
                         .addField('Calculated Focus Position', autoFocusData.CalculatedFocusPoint.Position, true);
 
                         
