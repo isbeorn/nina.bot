175798d4f44fe053e1512f3ef47b78d09209e0ce
Stefan Berg <isbeorn86@googlemail.com>
2020-06-09 15:27:22 +0200

    added stars help command

--- commands/HelpCommand.js
+++ commands/HelpCommand.js
@@ -111,8 +111,23 @@ class HelpCommand extends BaseCommand {
         break;
         
       }
+      case '!stars': {
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Where to find stars in N.I.N.A.?')   
+        .setURL('https://nighttime-imaging.eu/docs/develop/site/tabs/imaging/')
+        .setAuthor('Isbeorn', 'https://nighttime-imaging.eu/wp-content/uploads/2019/02/Logo_Nina.png')
+        .setThumbnail('https://nighttime-imaging.eu/wp-content/uploads/2019/02/Logo_Nina.png')
+        .setDescription(`
+        Stars are not part of the sky atlas search, as it only contains relevant deep sky objects.
+        Typically stars are only used for focusing. For this a small tool is available inside the imaging tab called "Manual Focus Targets". 
+        There you can find a list of the brightest stars in the nightsky ordered by altitude.
+        `)
+        .setImage('https://nighttime-imaging.eu/wp-content/uploads/2020/06/stars.png')
+        await message.reply(embed);
+        break;
+      }
       case '!help': {
-        await message.reply('Available commands: !donate, !logs, !docs, !devdocs, !tracker, !issue, !repository, !nina, !nina.docs, !troubleshoot, !conform, !32bitascom');
+        await message.reply('Available commands: !donate, !logs, !docs, !devdocs, !tracker, !issue, !repository, !nina, !nina.docs, !troubleshoot, !conform, !32bitascom, !stars');
         break;
       }
       default:
