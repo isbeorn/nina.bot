2f8209b19617562d83181c1b1694373ee0863795
Stefan Berg <isbeorn86@googlemail.com>
2020-06-23 17:07:15 +0200

    lbs and kg conversion

--- commands/MessageCommands/UnitConversionCommand.js
+++ commands/MessageCommands/UnitConversionCommand.js
@@ -3,21 +3,41 @@ const { MessageCommand } = require('./MessageCommand');
 
 class UnitConversionCommand extends MessageCommand {
     constructor() {
-        super(['!lbs']);
+        super(['!lbs', '!kg']);
     }
 
-    async process(message) {
-
+    async process(message) {        
         const parts = message.content.split(' ');
         if(parts.length > 1) {
             const value = parseFloat(parts[1]);
-            if(!isNaN(value)) {
-                const factor = 1/2.2046;
-    
-                await message.reply(`${value}lbs = ${mathjs.round(value * factor,2)} kg`)
+            let translated = NaN;
+            let baseUnit;
+            let translatedUnit;
+            switch(parts[0]) {
+                case '!lbs': {
+                    const factor = 1/2.2046;
+                    baseUnit = 'lbs';
+                    translatedUnit = 'kg'
+                    translated = this.translateByFactor(value, factor);
+                    break;
+                }
+                case '!kg': {
+                    const factor = 2.2046;
+                    baseUnit = 'kg';
+                    translatedUnit = 'lbs'
+                    translated = this.translateByFactor(value, factor);
+                    break;
+                }
             }
-            
+
+            if(!isNaN(translated)) {
+                await message.reply(`${value}${baseUnit} = ${translated}${translatedUnit}`)
+            }
         }
+    }
+
+    translateByFactor(value, factor, addition = 0) {
+        return mathjs.round(value * factor + addition,2);
     }
 }
 
