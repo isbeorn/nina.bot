7e9d70b68660755697c1150ffe95e18e1d3fe9a8
Stefan Berg <isbeorn86@googlemail.com>
2021-04-13 19:44:09 +0200

    added log location

--- commands/MessageCommands/SupportCommand.js
+++ commands/MessageCommands/SupportCommand.js
@@ -21,7 +21,7 @@ class SupportCommand extends MessageCommand {
                 1. Please describe what the issue is in detail
                 2. Give a detailed description on how to reproduce the problem step-by-step
                 3. Describe what the expected behavior should be and what the actual behavior is
-                4. Share your log file
+                4. Share your log file (located at "%LOCALAPPDATA%\\NINA\\Logs")
                   
                 Thank you.                
                 `
