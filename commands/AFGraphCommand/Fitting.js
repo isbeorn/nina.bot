9763bab870ffe893e31cd78030d3cbd33c805d36
Stefan Berg <isbeorn86@googlemail.com>
2022-04-18 04:22:15 +0200

    escape sqrt sign

--- commands/AFGraphCommand/Fitting.js
+++ commands/AFGraphCommand/Fitting.js
@@ -6,7 +6,7 @@ class Fitting {
 
         if (formula) {
             this.formula = formula;
-            this.fn = mathjs.compile(formula.replace(/²/g, '^2').replace(/³/g, '^3'));
+            this.fn = mathjs.compile(formula.replace(/²/g, '^2').replace(/³/g, '^3').replace(/√/g, 'sqrt'));
 
             const actualXs = measurePoints.map((x) => x.Position);
             const actualYs = measurePoints.map((x) => x.Value);
