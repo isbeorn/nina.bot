1d99fc7c8f48e0d786ba7c51305698f18802bf6a
Stefan Berg <isbeorn86@googlemail.com>
2025-08-23 09:40:37 +0200

    fix crash command text

--- commands/MessageCommands/CrashedCommand.js
+++ commands/MessageCommands/CrashedCommand.js
@@ -9,11 +9,11 @@ class CrashedCommand extends MessageCommand {
         const response = `
         **In case of application crashes**
 
-        When the application crashes the log file will most likely not contain any details about the crash, as the application is not able to write this info while crashing.
+When the application crashes the log file will most likely not contain any details about the crash, as the application is not able to write this info while crashing.
 
-        You can however check if a crash dump is available at \`%localappdata%\NINA\CrashDump\` and share it.
-        
-        If no crash dump is available you can also scan the windows event viewer. ('https://nighttime-imaging.eu/docs/master/site/troubleshooting/general/#event-viewer')
+You can however check if a crash dump is available at \`%localappdata%\\NINA\\CrashDump\` and share it.
+
+If no crash dump is available you can also scan the windows event viewer. ('https://nighttime-imaging.eu/docs/master/site/troubleshooting/general/#event-viewer')
                 `;
         
         await message.reply(response);
